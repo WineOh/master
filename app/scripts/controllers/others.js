98394cd1ce37ff60bfe76d1538bef6045504da4f
Paul Lee <plee@silverchalice.com>
2014-07-28 21:50:32 -0600

    latest ui styling for wineoh

---
+++ app/scripts/controllers/others.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('wineohApp')
+  .controller('OthersCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/whites.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('wineohApp')
+  .controller('WhitesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
